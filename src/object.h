b5cafaabcbaa92a672ed0cfef443a7970a410f98
IMSDcrueoft <imsd_personal@outlook.com>
2025-05-13 22:50:10 +0800

    fix lru table

--- src/object.h
+++ src/object.h
@@ -150,13 +150,14 @@ typedef struct {
 } ObjClass;
 
 //uint8_t inline access_slot(uint8_t lru_state, uint8_t slot) {
-//	const uint8_t lru_transition[6][3] = {
-//		/* 0:0,1,2 */ {0,2,4},
-//		/* 1:0,2,1 */ {0,3,1},
-//		/* 2:1,0,2 */ {2,1,5},
-//		/* 3:1,2,0 */ {3,1,3},
-//		/* 4:2,0,1 */ {4,5,4},
-//		/* 5:2,1,0 */ {5,5,5},
+//	const uint8_t lru_transition[9][3] = {
+//		/* [replace,mid,recent] ,the recent-slot will move to back */
+//		/* 0:0,1,2 */ {3,1,0},
+//		/* 1:0,2,1 */ {5,1,0},
+//		/* 2:1,0,2 */ {3,1,2},
+//		/* 3:1,2,0 */ {3,4,2},
+//		/* 4:2,0,1 */ {5,4,0},
+//		/* 5:2,1,0 */ {5,4,2},
 //	};
 //
 //	return lru_transition[lru_state][slot];
