5d2b476ed32edfe370bdbfcc5138922b1571b70f
IMSDcrueoft <imsd_personal@outlook.com>
2025-05-14 21:52:44 +0800

    add clox "clock" fun

--- src/nativeGlobal.c
+++ src/nativeGlobal.c
@@ -6,10 +6,18 @@
 #include "nativeBuiltin.h"
 #include "vm.h"
 
+//#include <time.h>
+//static Value clockNative(int argCount, Value* args) {
+//	return NUMBER_VAL((double)clock() / CLOCKS_PER_SEC);
+//}
+
 //place your function here
 
 COLD_FUNCTION
 void importNative_global()
 {
+	// origion clox function
+	// defineNative_global("clock", clockNative);
+
 	//nothing here yet.
 }
